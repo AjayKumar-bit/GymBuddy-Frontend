8a9e62bfa7786423bd88a95d5ba8ce7f378f7c2f
AjayKumar-bit <Ajaykumar.mca22@gmail.com>
2024-11-03 16:35:11 +0530

    fastImage : Fast Image component with shimmer loadding effect

---
+++ src/components/fast-image/gbFastImage.styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+import { SCREEN_HEIGHT } from '@constants'
+
+const styles = StyleSheet.create({
+  image: {
+    height: SCREEN_HEIGHT * 0.22,
+    width: '100%',
+  },
+})
+
+export { styles }
