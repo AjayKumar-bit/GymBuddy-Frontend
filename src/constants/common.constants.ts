7a1bc17b4188c15661d19166d3ca97fb737a8ced
AjayKumar-bit <Ajaykumar.mca22@gmail.com>
2024-11-04 15:00:50 +0530

    text-input : Custom Text Input Component

--- src/constants/common.constants.ts
+++ src/constants/common.constants.ts
@@ -7,3 +7,12 @@ export const SUPPORTED_LANGUAGE = {
 }
 export const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get('window')
 export const TAB_BAR_HEIGHT = 60
+export const HIT_SLOP_FIVE = { top: 5, bottom: 5, left: 5, right: 5 }
+
+// Enums 
+export enum TextInputPreset {
+  Default = 'default',
+  Email = 'e-mail',
+  Password = 'password',
+  Search = 'search',
+}
--- src/theme/palettes.ts
+++ src/theme/palettes.ts
@@ -1,5 +1,12 @@
 export enum Palette {
+  AerieBlack = '#1A1A1A',
   Black = '#000000',
+  ChineseWhite = '#E0E0E0',
   Cyan = '#00B6DF',
+  DimGray = '#6C6C6C',
   DolphinGray = '#7E8A8C',
+  Gray = '#808181',
+  InternationalOrange = '#CD2504',
+  JungleGreen = '#193238',
+  LightSilver = '#D7D7D7',
 }
