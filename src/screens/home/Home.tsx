6cbd96f095a565cee1715619058d84e6bbc03295
AjayKumar-bit <Ajaykumar.mca22@gmail.com>
2024-11-05 16:56:41 +0530

    components : Component Absolute import updated

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-import GBAppHeader from '../../components/app-header/GBAppHeader'
+import { GBAppHeader } from '@components'
 
 const Home = () => {
   return (
